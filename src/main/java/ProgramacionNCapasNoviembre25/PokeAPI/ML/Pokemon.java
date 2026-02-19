92dd7c7f3c5a36a69ff90b17f6de33ab9ec7c827
Oswaldo Benitez Camacho <oswaldobenitez01@outlook.com>
2026-02-18 18:52:04 -0600

    Iniciando modelo de pokemon

---
+++ src/main/java/ProgramacionNCapasNoviembre25/PokeAPI/ML/Pokemon.java
@@ -0,0 +1,12 @@
+
+package ProgramacionNCapasNoviembre25.PokeAPI.ML;
+
+public class Pokemon {
+    private Integer id;
+    private String name;
+    private Integer baseExperience;
+    private Integer height;
+    private Boolean isDefault;
+    private Integer order;
+    private Integer weight;
+}
