d8db3b51d26d500052c219cc15d1ec20c1daa5d4
Oswaldo Benitez Camacho <oswaldobenitez01@outlook.com>
2026-02-19 13:03:28 -0600

    Haciendo controlador de prueba

--- src/main/java/ProgramacionNCapasNoviembre25/PokeAPI/Controller/PokeApiController.java
+++ src/main/java/ProgramacionNCapasNoviembre25/PokeAPI/Controller/PokeApiController.java
@@ -1,11 +1,16 @@
+// Controller/PokemonController.java
 package ProgramacionNCapasNoviembre25.PokeAPI.Controller;
 
 import ProgramacionNCapasNoviembre25.PokeAPI.ML.Pokemon;
+import ProgramacionNCapasNoviembre25.PokeAPI.ML.PokemonListResponse;
+import ProgramacionNCapasNoviembre25.PokeAPI.ML.Result;
 import ProgramacionNCapasNoviembre25.PokeAPI.Service.PokemonService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
+// Controller/PokemonController.java
 @Controller
 @RequestMapping("/pokemon")
 public class PokeApiController {
@@ -16,15 +21,26 @@ public class PokeApiController {
         this.pokemonService = pokemonService;
     }
 
-    @GetMapping("/mostrar")
-    public Pokemon obtenerPokemon() {
+    @GetMapping("/list")
+    public String pokemonList(
+            @RequestParam(defaultValue = "20") int limit,
+            @RequestParam(defaultValue = "0") int offset,
+            Model model) {
 
-        Pokemon pokemon = pokemonService.getPokemon();
+        Result result = pokemonService.getPokemonList(limit, offset);
         
-        System.out.println("El Pokemon obtenido es: " + pokemon);
-
+        model.addAttribute("result", result);
+        model.addAttribute("nextOffset", offset + limit);
         
-        return pokemon;
+        return "pokemon/list";
     }
 
+    @GetMapping("/{idOrName}")
+    public String pokemonDetail(@PathVariable String idOrName, Model model) {
+        
+        Result result = pokemonService.getPokemonByIdOrName(idOrName);
+        model.addAttribute("result", result);
+        
+        return "pokemon/detail";
+    }
 }
---
+++ src/main/java/ProgramacionNCapasNoviembre25/PokeAPI/ML/PokemonListResponse.java
@@ -0,0 +1,56 @@
+package ProgramacionNCapasNoviembre25.PokeAPI.ML;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PokemonListResponse {
+
+    @JsonProperty("count")
+    private Integer count;
+
+    @JsonProperty("next")
+    private String next;
+
+    @JsonProperty("previous")
+    private String previous;
+
+    @JsonProperty("results")
+    private List<NamedResource> results;
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public String getNext() {
+        return next;
+    }
+
+    public void setNext(String next) {
+        this.next = next;
+    }
+
+    public String getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(String previous) {
+        this.previous = previous;
+    }
+
+    public List<NamedResource> getResults() {
+        return results;
+    }
+
+    public void setResults(List<NamedResource> results) {
+        this.results = results;
+    }
+    
+    
+}
+
