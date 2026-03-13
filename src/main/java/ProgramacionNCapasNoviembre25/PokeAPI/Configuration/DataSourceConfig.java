1dcb7ed2f3a0a16686aca6c0a916f72093117310
Alien 13 <Alien 13@192.167.0.63>
2026-03-13 12:54:49 -0600

    Actualizacion ip

--- src/main/java/ProgramacionNCapasNoviembre25/PokeAPI/Configuration/DataSourceConfig.java
+++ src/main/java/ProgramacionNCapasNoviembre25/PokeAPI/Configuration/DataSourceConfig.java
@@ -14,7 +14,7 @@ public class DataSourceConfig {
     public DataSource DataSource(){
     
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUrl("jdbc:oracle:thin:@192.167.1.40:1521:orcl");
+        dataSource.setUrl("jdbc:oracle:thin:@192.167.0.228:1521:orcl");
         dataSource.setUsername("PokeApiNoviembre2025");
         dataSource.setPassword("password1");
         
